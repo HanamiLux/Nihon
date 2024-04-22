269b63aa0cb52b393aa9f928ee2c3e023b4f7b4b
HanamiSHLux <oxotnikzn2017@yandex.ru>
2024-04-22 03:19:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<img src="app/src/main/res/drawable-nodpi/logo.png" align=center>
+
+____
+
+<div align=center>
+<img src="https://img.shields.io/github/repo-size/HanamiLux/NihonHistory" alt="repoSize"/>
+<img src="https://img.shields.io/github/downloads/HanamiLux/NihonHistory/total" alt="downloads"/>
+<img src="https://img.shields.io/github/last-commit/HanamiLux/NihonHistory" alt="lastCommit"/>
+</div>
+
+
+<h2 align=center>History & culture of Japan in one app!</h2>
+
+## **Installation**
+
+- Soon
+
+## **Usage**
+
+Soon
+
+
+## **Show your support**
+Leave a ⭐ if you like this project
---
+++ app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.compilerKsp)
+}
+
+android {
+    namespace = "com.example.nihonhistory"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.nihonhistory"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    //Room
+    ksp(libs.androidx.room.ksp)
+    implementation(libs.androidx.room)
+    implementation(libs.androidx.room.ktx)
+
+    //MVVM Architecture
+    implementation(libs.androidx.lifecycle.viewmodel)
+    //Coroutines
+    implementation(libs.kotlinx.coroutines)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
