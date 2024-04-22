3d1d624b0ba5ea69d29591420870effe91aab770
HanamiLux <oxotnikzn2017@yandex.ru>
2024-04-22 17:44:44 +0300

    sign in/up/recovery ui

---
+++ app/src/main/java/com/example/nihonhistory/PasswordRecoveryActivity.kt
@@ -0,0 +1,20 @@
+package com.example.nihonhistory
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PasswordRecoveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_password_recovery)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
